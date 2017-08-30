b30cd2ca1e35d696703a972ecdf1c34932fecf18
DragonFerocity <rtanq9@mst.edu>
2017-08-30 16:26:20 -0500

    Initial Commit

---
+++ Assets/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+  public abstract void Start();
+  public abstract void Update();
+  void jump()
+  {
+    //Jump Code here
+  }
+}
---
+++ Assets/EntityPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityPlayer : Entity {
+
+	// Use this for initialization
+	public override void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+    move();
+	}
+
+  private void move()
+  {
+    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 15.0f; 
+    var y = Input.GetAxis("Vertical") * Time.deltaTime * 15.0f;
+
+    transform.Translate(x, y, 0);
+  }
+}
