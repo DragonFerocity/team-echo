391fcd3cb5e40a1a72f9f7e73248e1ba13c3b415
Kyle Knudsen <kky47@mst.edu>
2017-10-09 14:31:25 -0500

    Fixed UI Errors

--- Assets/UIControls.cs
+++ Assets/UIControls.cs
@@ -6,26 +6,45 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class UIControls : MonoBehaviour {
 
-    public Canvas inGameMenu;
+    public Canvas inGameMainMenu;
+    public Canvas inGameLevelsMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        inGameMainMenu = inGameMainMenu.GetComponent<Canvas>();
+        inGameMainMenu.enabled = false;
+        inGameLevelsMenu.enabled = false;
+    }
 
     public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
 
-    // Use this for initialization
-    void Start () {
-        inGameMenu = inGameMenu.GetComponent<Canvas>();
-        inGameMenu.enabled = false;
-	}
-	
 
-	// Update is called once per frame
-	void Update () {
+    public void MainMenuToLevelMenu()
+    {
+        inGameMainMenu.enabled = false;
+        inGameLevelsMenu.enabled = true;
+    }
+
+    public void LevelMenuToMainMenu()
+    {
+        inGameLevelsMenu.enabled = false;
+        inGameMainMenu.enabled = true;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
         if (CrossPlatformInputManager.GetButtonDown("Main Menu"))
         {
-            inGameMenu.enabled = !inGameMenu.enabled;
-
+            inGameMainMenu.enabled = !inGameMainMenu.enabled;
+            if (inGameLevelsMenu.enabled == true)
+            {
+                inGameLevelsMenu.enabled = false;
+            }  
         }
     }
 }
